9e5d8e125c7e1aef9eb871330a7d9a0ee633d83c
Brianca Knight <briancaknight@gmail.com>
2024-01-22 10:52:55 -0800

    add and connect form to madlib
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -9,23 +9,40 @@ namespace MadLibs.Controllers
     public ActionResult BlankStory() { return View(); }
 
     [Route("/finalStory")]
-    public ActionResult FinalStory()
+    public ActionResult FinalStory(string adjective1, string adjective2, string adjective3, string adjective4, string properNoun1, string properNoun2, string properNoun3, string verb1, string verb2, string noun)
     {
       FinalStoryVariable myFinalStoryVariable = new FinalStoryVariable();
-      myFinalStoryVariable.Adjective1 = "word";
-      myFinalStoryVariable.Adjective2 = "word";
-      myFinalStoryVariable.Adjective3 = "word";
-      myFinalStoryVariable.Adjective4 = "word";
-      myFinalStoryVariable.ProperNoun1 = "word";
-      myFinalStoryVariable.ProperNoun2 = "word";
-      myFinalStoryVariable.ProperNoun3 = "word";
-      myFinalStoryVariable.Verb1 = "word";
-      myFinalStoryVariable.Verb2 = "word";
-      myFinalStoryVariable.Noun = "word";
+      myFinalStoryVariable.Adjective1 = adjective1;
+      myFinalStoryVariable.Adjective2 = adjective2;
+      myFinalStoryVariable.Adjective3 = adjective3;
+      myFinalStoryVariable.Adjective4 = adjective4;
+      myFinalStoryVariable.ProperNoun1 = properNoun1;
+      myFinalStoryVariable.ProperNoun2 = properNoun2;
+      myFinalStoryVariable.ProperNoun3 = properNoun3;
+      myFinalStoryVariable.Verb1 = verb1;
+      myFinalStoryVariable.Verb2 = verb2;
+      myFinalStoryVariable.Noun = noun;
       return View(myFinalStoryVariable);
     }
 
-    // [Route("/")]
-    // public string HomePage() { return "Home"; }
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    // [Route("/libs")]
+    // public ActionResult Libs(string Adjective1, string Adjective2, string Adjective3, string Adjective4, string ProperNoun1, string ProperNoun2, string ProperNoun3, string Verb1, string Verb2, string Noun)
+    // {
+    //   FinalStoryVariable myFinalStoryVariable = new FinalStoryVariable();
+    //   myFinalStoryVariable.Adjective1 = adjective1;
+    //   myFinalStoryVariable.Adjective2 = adjective2;
+    //   myFinalStoryVariable.Adjective3 = adjective3;
+    //   myFinalStoryVariable.Adjective4 = adjective4;
+    //   myFinalStoryVariable.ProperNoun1 = properNoun1;
+    //   myFinalStoryVariable.ProperNoun2 = properNoun2;
+    //   myFinalStoryVariable.ProperNoun3 = properNoun3;
+    //   myFinalStoryVariable.Verb1 = verb1;
+    //   myFinalStoryVariable.Verb2 = verb2;
+    //   myFinalStoryVariable.Noun = noun;
+    //   return View(myFinalStoryVariable);
+    // }
   }
 }
